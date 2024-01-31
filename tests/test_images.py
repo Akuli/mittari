23bae6b7efdbe2abf486d508d8179ac5d389ece0
Akuli <akuviljanen17@gmail.com>
2024-01-31 14:23:33 +0200

    Add another image test

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,6 +1,7 @@
 import shutil
 import subprocess
 import glob
+import os
 
 import pytest
 
@@ -16,3 +17,9 @@ def test_jpg_sizes(image_filename):
     width, height = map(int, size.split("x"))
     assert width <= 1000
     assert height <= 1000
+
+
+def test_resized_image_exists(image_filename):
+    for filename in os.listdir("images/large-to-be-resized-with-script"):
+        if not os.path.exists(f"images/{image_filename}"):
+            raise Exception("you need to run images/resize.sh")
