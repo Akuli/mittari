5c7f0ec53cc3b5518fcdec7d6655cce3ba0fcdb1
Akuli <akuviljanen17@gmail.com>
2024-01-31 14:06:48 +0200

    check image sizes in CI

---
+++ tests/test_images.py
@@ -0,0 +1,18 @@
+import shutil
+import subprocess
+import glob
+
+import pytest
+
+
+@pytest.mark.skipif(
+    shutil.which("file") is None,
+    reason="imagemagic not installed, it's needed to find image sizes"
+)
+@pytest.mark.parametrize("image_filename", glob.glob("images/*.jpg"))
+def test_jpg_sizes(image_filename):
+    output = subprocess.check_output(["identify", image_filename], text=True)
+    size = output.split()[2]
+    width, height = map(int, size.split("x"))
+    assert width <= 1000
+    assert height <= 1000
